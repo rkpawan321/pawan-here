0c47af35a591f658d51ad9729b82226c64af9ca6
rkpawan321 <rkpawan321@gmail.com>
2024-02-26 23:35:12 -0700

    added 3D shape on right side

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,6 +5,8 @@ import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
 import Bounded from "@/app/components/Bounded";
+import Shapes from "./Shapes";
+
 /**
  * Props for `Hero`.
  */
@@ -70,6 +72,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
             <span className="block text-slate-300">
