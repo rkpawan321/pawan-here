c01e1618bf30ae45ca25ed1b45526649c9ce78d6
rkpawan321 <rkpawan321@gmail.com>
2024-03-12 23:59:19 -0700

    adding biography page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default async function Header() {
     const client = createClient();
     const settings = await client.getSingle("settings");
+
     return (
         <header className="top-0 z-50 mx-auto max-w-7x1 md:sticky md:top-4">
             <nav>
